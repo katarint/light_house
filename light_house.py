04021a3ff292f1386f17a22f4ea8f4934a410b25
katarint <35830424+katarint@users.noreply.github.com>
2018-02-01 21:31:41 +0100

    Initial commit

---
+++ light_house.py
@@ -0,0 +1,17 @@
+
+# defines the function light_house , input parameters : (x_0,y_0,N)
+# x,y= position of light house, N=number of sampled data
+
+import numpy as np
+
+def light_house(x_0, y_0, N):
+
+
+    np.array=np.random.uniform(0, np.pi, size=N)
+
+    return;
+
+light_house(1,1,5)
+
+print('*********************************************************************************************************')
+print('data samples (in angles):',np.array)
