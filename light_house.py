f3310a1edf55282b6904394a366d579ae3a14c4d
katarint <35830424+katarint@users.noreply.github.com>
2018-02-03 18:35:47 +0100

    final result

--- light_house.py
+++ light_house.py
@@ -1,18 +1,57 @@
 
-# defines the function light_house , input parameters : (x_0,y_0,N)
-# x,y= position of light house, N=number of sampled data
-
 import numpy as np
+import matplotlib.pyplot as plt
 
-def light_house(x_0, y_0, N):
+# Generates a vector containing random angles ranging from 0 to pi
+def random_angle_generator(N):
+    angle_array = np.random.uniform(0, np.pi, size=N)
+    return angle_array
 
 
-    np.array=np.random.uniform(0, np.pi, size=N)
+# converts the generated angles into x-positions
+def position_converter(angle_array,x_0,y_0):
+    temp = 0
 
-    return;
+    for i in range(len(angle_array)):
+        if angle_array[i-temp] == 0:
+            del angle_array[i-temp]
+            temp += 1
 
-light_house(1,1,5)
+    hypotenuse_array = np.zeros(len(angle_array))    #hypotenuse_arrays contains all the values of the hypotenuse of data
+    abs_x_array = np.zeros(len(angle_array))
+    position_array = np.zeros(len(angle_array))
 
-print('*********************************************************************************************************')
-print('data samples (in angles):',np.array)
-print('testing')
+    for i in range(len(angle_array)):
+        if angle_array[i] <= np.pi/2:
+            hypotenuse_array[i]=y_0/np.sin(angle_array[i])
+            abs_x_array[i]=hypotenuse_array[i]*np.cos(angle_array[i])
+            position_array[i]=x_0-abs_x_array[i]
+
+        elif angle_array[i] > np.pi/2:
+            angle_array[i] = np.pi-angle_array[i]
+            hypotenuse_array[i] = y_0 / np.sin(angle_array[i])
+            abs_x_array[i] = hypotenuse_array[i] * np.cos(angle_array[i])
+            position_array[i] = x_0 + abs_x_array[i]
+
+    return position_array
+
+
+
+def light_house(x_0,y_0,N):
+       angle_array=random_angle_generator(N)
+       position_array=position_converter(angle_array,x_0,y_0)
+       plt.hist(position_array, 'fd', range=None, normed=False, weights=None, density=None)  # determining bin size
+                                                                                             # with Freedman-method
+       plt.ylabel('Number of counts')
+       plt.xlabel('Measurement variable')
+       plt.title('Histogram of data points')
+       plt.show()
+
+#######################################################################################################################
+# light_house-function input parameters : (x_0,y_0,N)
+# x_0,y_0= position of light house, N = number of data
+
+light_house(50,10,500)
+
+
+
